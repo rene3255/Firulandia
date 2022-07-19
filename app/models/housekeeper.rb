8e3dd65d355762fc20f7f7a6131a60408cc6a0ab
claunicole <claunicole.gf@gmail.com>
2022-07-19 13:35:39 -0400

    add dotenv-rails

--- app/models/housekeeper.rb
+++ app/models/housekeeper.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: housekeepers
+#
+#  id          :integer          not null, primary key
+#  address     :string           not null
+#  conditions  :text             not null
+#  description :text             not null
+#  full_name   :string           not null
+#  price       :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer          not null
+#
+# Indexes
+#
+#  index_housekeepers_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  user_id  (user_id => users.id)
+#
 class Housekeeper < ApplicationRecord
   belongs_to :user
 end
--- test/models/booking_test.rb
+++ test/models/booking_test.rb
@@ -1,3 +1,30 @@
+# == Schema Information
+#
+# Table name: bookings
+#
+#  id              :integer          not null, primary key
+#  confirmed       :boolean          default(FALSE)
+#  end_date        :date             not null
+#  full_name       :string           not null
+#  pet_description :text             not null
+#  price           :integer          not null
+#  review          :text
+#  start_date      :date             not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  housekeeper_id  :integer          not null
+#  user_id         :integer          not null
+#
+# Indexes
+#
+#  index_bookings_on_housekeeper_id  (housekeeper_id)
+#  index_bookings_on_user_id         (user_id)
+#
+# Foreign Keys
+#
+#  housekeeper_id  (housekeeper_id => housekeepers.id)
+#  user_id         (user_id => users.id)
+#
 require "test_helper"
 
 class BookingTest < ActiveSupport::TestCase
